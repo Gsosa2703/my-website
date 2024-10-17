cb65f6616b3c478e67c5eb4f7edfcef59a50d26f
Gabriela <gabriela.sosa@blackboxintelligence.com>
2024-10-17 00:06:33 -0300

    chore: navbar scrolling working

---
+++ app/components/Header.tsx
@@ -0,0 +1,84 @@
+// app/components/Header.tsx
+const Header: React.FC = () => {
+ return (
+  <header className="py-10">
+   <div className="max-w-7xl mx-auto text-center">
+    <h1 className="text-5xl font-bold">
+     Welcome to My Website
+    </h1>
+    <p className="mt-4">
+     We provide the best services in the industry.
+    </p>
+    <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p>
+    <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p>
+    <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p> <p className="mt-4">
+     We provide the best services in the industry.
+    </p><p className="mt-4">
+     We provide the best services in the industry.
+    </p><p className="mt-4">
+     We provide the best services in the industry.
+    </p><p className="mt-4">
+     We provide the best services in the industry.
+    </p><p className="mt-4">
+     We provide the best services in the industry.
+    </p><p className="mt-4">
+     We provide the best services in the industry.
+    </p><p className="mt-4">
+     We provide the best services in the industry.
+    </p><p className="mt-4">
+     We provide the best services in the industry.
+    </p><p className="mt-4">
+     We provide the best services in the industry.
+    </p><p className="mt-4">
+     We provide the best services in the industry.
+    </p><p className="mt-4">
+     We provide the best services in the industry.
+    </p>
+    <a
+     href="#"
+     className="mt-6 inline-block py-3 px-6 rounded-lg hover:bg-blue-700"
+    >
+     Get Started
+    </a>
+   </div>
+  </header>
+ );
+};
+
+export default Header;
